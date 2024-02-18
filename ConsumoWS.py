a0c7a24f168c73680cb22bc36396f312e0e8dd32
MiguelChavesm <miguelchavesm0597@gmail.com>
2024-02-18 13:13:47 -0500

    Se configura que el sistema esté siempre esperando archivos

--- ConsumoWS.py
+++ ConsumoWS.py
@@ -2,159 +2,220 @@ import requests
 from requests.auth import HTTPBasicAuth
 import os
 from datetime import datetime
+import tkinter as tk
+from tkinter import ttk, messagebox
+from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
+from threading import Thread
+import time
 
-# URL del servicio de acceso a tokens
-token_url = "https://mingle-sso.inforcloudsuite.com:443/NUGH6DGWYB5E8AMU_TST/as/token.oauth2"
-
-# Información de autenticación
-client_id = "NUGH6DGWYB5E8AMU_TST~_tOirRI-jy9pzu4Xun2ESJvNqMVTHg_jJntMDzFtgV0"
-client_secret = "ptQu3maUMRAlScq_xe2-mZLsJkPtT_fkrDWTOGEVJreUHyPqavPhncXtX1cRCVE8uNSQei4CQO0xqssZvwgU9A"
-username = "NUGH6DGWYB5E8AMU_TST#ktzJTSlcIfY9X5sH9tUacghKkC7n7TLZXCgx51jQyHjPXJvxzarlQsufPAusg4XgDa6GbLvXKcKvjwN7ljHBlg"
-password = "jxy5rCtcwN_jf0b8R1Cbe2FxkBQ-paCjmDwspfGqu7E1Mwj0SsDneZKBF41g4alWZ-lTUWCRl0p7M8tJ0yVknA"
-
-# Ruta de la carpeta donde se encuentran los archivos txt
-carpeta_archivos = "Data_mavesa/"
-
-# Ruta de la carpeta "procesados"
-carpeta_procesados = "Procesados/"
-
-def enviar_data(data): 
-# Cuerpo del JSON
-
-    # Obtener token de acceso
-    token_response = requests.post(
-        token_url,
-        auth=HTTPBasicAuth(client_id, client_secret),
-        data={
-            "grant_type": "password",
-            "username": username,
-            "password": password
-        }
-    )
-
-    if token_response.status_code == 200:
-        access_token = token_response.json().get("access_token")
-        #print(access_token)
-
+class ProcesadorArchivos:
+    def __init__(self,root):
+            
+        self.root = root
+        self.root.title("MONTRA")
         
-        # Si se obtiene el token, realizar la solicitud POST con el cuerpo JSON
-        if access_token:
-            headers = {
-                "Authorization": f"Bearer {access_token}",
-                "Content-Type": "application/json"
+        self.notebook = ttk.Notebook(root)
+        self.notebook.pack(fill="both", expand=True)
+        self.medicion_tab = ttk.Frame(self.notebook)
+        self.configuracion_tab = ttk.Frame(self.notebook)
+
+        self.notebook.add(self.medicion_tab, text="Respuesta y DB", state="normal")  # Inicialmente deshabilitada
+        self.notebook.add(self.configuracion_tab, text="Configuración", state="normal")  # Inicialmente deshabilitada
+        
+        self.create_medicion_tab()
+        self.create_configuracion_tab()
+        
+        # URL del servicio de acceso a tokens
+        self.token_url = "https://mingle-sso.inforcloudsuite.com:443/NUGH6DGWYB5E8AMU_TST/as/token.oauth2"
+
+        # Información de autenticación
+        self.client_id = "NUGH6DGWYB5E8AMU_TST~_tOirRI-jy9pzu4Xun2ESJvNqMVTHg_jJntMDzFtgV0"
+        self.client_secret = "ptQu3maUMRAlScq_xe2-mZLsJkPtT_fkrDWTOGEVJreUHyPqavPhncXtX1cRCVE8uNSQei4CQO0xqssZvwgU9A"
+        self.username = "NUGH6DGWYB5E8AMU_TST#ktzJTSlcIfY9X5sH9tUacghKkC7n7TLZXCgx51jQyHjPXJvxzarlQsufPAusg4XgDa6GbLvXKcKvjwN7ljHBlg"
+        self.password = "jxy5rCtcwN_jf0b8R1Cbe2FxkBQ-paCjmDwspfGqu7E1Mwj0SsDneZKBF41g4alWZ-lTUWCRl0p7M8tJ0yVknA"
+
+        # Ruta de la carpeta donde se encuentran los archivos txt
+        self.carpeta_archivos = "Data_mavesa/"
+
+        # Ruta de la carpeta "procesados"
+        self.carpeta_procesados = "Procesados/"
+
+        # Variable para controlar la ejecución del programa
+        self.ejecutar = True
+
+
+
+    def create_medicion_tab(self):
+        # Botón "Iniciar"
+        self.boton_iniciar = tk.Button(self.medicion_tab, text="Iniciar", command=self.iniciar_proceso)
+        self.boton_iniciar.pack(pady=10)
+
+        # Botón "Detener"
+        self.boton_detener = tk.Button(self.medicion_tab, text="Detener", command=self.detener_proceso)
+        self.boton_detener.pack(pady=10)
+
+    def create_configuracion_tab(self):
+        ttk.Label(self.configuracion_tab, text="Hola")
+
+    def enviar_data(self, data):
+        # Cuerpo del JSON
+
+        # Obtener token de acceso
+        token_response = requests.post(
+            self.token_url,
+            auth=HTTPBasicAuth(self.client_id, self.client_secret),
+            data={
+                "grant_type": "password",
+                "username": self.username,
+                "password": self.password
             }
+        )
 
-            api_url = "https://mingle-ionapi.inforcloudsuite.com/NUGH6DGWYB5E8AMU_TST/WM/wmwebservice_rest/NUGH6DGWYB5E8AMU_TST_ENTERPRISE/packs"
+        if token_response.status_code == 200:
+            access_token = token_response.json().get("access_token")
+            # print(access_token)
 
-            response = requests.post(api_url, json=data, headers=headers)
+            # Si se obtiene el token, realizar la solicitud POST con el cuerpo JSON
+            if access_token:
+                headers = {
+                    "Authorization": f"Bearer {access_token}",
+                    "Content-Type": "application/json"
+                }
 
-            if response.status_code == 200:
-                print("Solicitud exitosa:", response.json())
+                api_url = "https://mingle-ionapi.inforcloudsuite.com/NUGH6DGWYB5E8AMU_TST/WM/wmwebservice_rest/NUGH6DGWYB5E8AMU_TST_ENTERPRISE/packs"
+
+                response = requests.post(api_url, json=data, headers=headers)
+
+                if response.status_code == 200:
+                    print("Solicitud exitosa:", response.json())
+                else:
+                    print("Error en la solicitud:", response.text)
             else:
-                print("Error en la solicitud:", response.text)
+                print("No se pudo obtener el token de acceso.")
         else:
-            print("No se pudo obtener el token de acceso.")
-    else:
-        print("Error al obtener el token de acceso:", token_response.text)
+            print("Error al obtener el token de acceso:", token_response.text)
 
-def procesar_archivo(archivo):
-    try:
-        with open(archivo, "r") as f:
-            line = f.readline().strip()
-            datos = line.split("|")
+    def procesar_archivo(self, archivo):
+        try:
+            with open(archivo, "r") as f:
+                line = f.readline().strip()
+                datos = line.split("|")
 
-            SKU, Packtype, Tipodepaquete, Cantidad ,Largo, Ancho, Alto, Peso, Descripcion = datos
+                SKU, Packtype, Tipodepaquete, Cantidad, Largo, Ancho, Alto, Peso, Descripcion = datos
 
-            # Convertir los valores a números (Largo, Ancho, Alto y Peso)
-            Largo = float(Largo)
-            Ancho = float(Ancho)
-            Alto = float(Alto)
-            Peso = float(Peso)
-
-            # Realizar cálculos o lógica de procesamiento aquí
-
-            # Puedes imprimir los datos procesados
-            print(f"SKU: {SKU}")
-            print(f"Packtype: {Packtype}")
-            print(f"Tipodepaquete: {Tipodepaquete}")
-            print(f"Cantidad: {Cantidad}")
-            print(f"Largo: {Largo}")
-            print(f"Ancho: {Ancho}")
-            print(f"Alto: {Alto}")
-            print(f"Peso: {Peso}")
-            print(f"Descripcion: {Descripcion}")
-
-            if Packtype=="Unidad-UOM3":
-                data = {
-                    "packkey": f"{SKU}_{Cantidad}",
-                    "packdescr": Descripcion,
-                    "packuom1": "CJ",
-                    "packuom3": Tipodepaquete,
-                    "casecnt": Cantidad,
-                    "qty": 1,
-                    "widthuom3": Ancho,
-                    "lengthuom3": Largo,
-                    "heightuom3": Alto,
-                    "weightuom3": Peso,
-                    "widthuom1":  Ancho,
-                    "lengthuom1": Largo,
-                    "heightuom1": Alto,
-                    "weightuom1": Peso,
-                    "pallethi": 1,
-	                "palletti": 1,
-                 	"ext_udf_str1": SKU,
-	                "ext_udf_str2": Tipodepaquete
-                }
-                print (data)
-                enviar_data(data)
-            elif Packtype=="Caja-UOM1":
-                data = {
-                    "packkey": f"{SKU}_{Cantidad}",
-                    "packdescr": Descripcion,
-                    "packuom1": Tipodepaquete,
-                    "casecnt": Cantidad,
-                    "qty": 1,
-                    "widthuom1":  Ancho,
-                    "lengthuom1": Largo,
-                    "heightuom1": Alto,
-                    "weightuom1": Peso,
-                    "pallethi": 1,
-	                "palletti": 1,
-                 	"ext_udf_str1": SKU,
-	                "ext_udf_str2": Tipodepaquete
-                }
-                print (data)
-                enviar_data(data)
-        
-        
-            # Mueve el archivo procesado a la carpeta "procesados"
-
-            print(f"Archivo procesado: {archivo}")
-
-    except Exception as e:
-        print(f"Error al procesar el archivo {archivo}: {str(e)}")
-
-def obtener_archivo_mas_antiguo(carpeta):
-    archivos = [f for f in os.listdir(carpeta) if f.endswith(".txt")]
-    if not archivos:
-        return None
-
-    archivos.sort(key=lambda x: datetime.strptime(x.split("_")[1].replace('.txt', ''), "%Y%m%d%H%M%S"))
-    return os.path.join(carpeta, archivos[0])
-
-while True:
-    archivo = obtener_archivo_mas_antiguo(carpeta_archivos)
-    if archivo:
-        procesar_archivo(archivo)
-        
-        # Verifica si el archivo todavía existe antes de intentar moverlo
-        if os.path.exists(archivo):
-            nuevo_nombre = os.path.join(carpeta_procesados, os.path.basename(archivo))
-            os.rename(archivo, nuevo_nombre)
-        else:
-            print(f"El archivo {archivo} ya no existe. No se puede mover.")
-    else:
-        print("No hay más archivos por procesar.")
-        break
+                # Convertir los valores a números (Largo, Ancho, Alto y Peso)
+                Largo = float(Largo)
+                Ancho = float(Ancho)
+                Alto = float(Alto)
+                Peso = float(Peso)
 
 
+                # Puedes imprimir los datos procesados
+                print(f"SKU: {SKU}")
+                print(f"Packtype: {Packtype}")
+                print(f"Tipodepaquete: {Tipodepaquete}")
+                print(f"Cantidad: {Cantidad}")
+                print(f"Largo: {Largo}")
+                print(f"Ancho: {Ancho}")
+                print(f"Alto: {Alto}")
+                print(f"Peso: {Peso}")
+                print(f"Descripcion: {Descripcion}")
+
+                if Packtype == "Unidad-UOM3":
+                    data = {
+                        "packkey": f"{SKU}_{Cantidad}",
+                        "packdescr": Descripcion,
+                        "packuom1": "CJ",
+                        "packuom3": Tipodepaquete,
+                        "casecnt": Cantidad,
+                        "qty": 1,
+                        "widthuom3": Ancho,
+                        "lengthuom3": Largo,
+                        "heightuom3": Alto,
+                        "weightuom3": Peso,
+                        "widthuom1": Ancho,
+                        "lengthuom1": Largo,
+                        "heightuom1": Alto,
+                        "weightuom1": Peso,
+                        "pallethi": 1,
+                        "palletti": 1,
+                        "ext_udf_str1": SKU,
+                        "ext_udf_str2": Tipodepaquete
+                    }
+                    print(data)
+                    self.enviar_data(data)
+                elif Packtype == "Caja-UOM1":
+                    data = {
+                        "packkey": f"{SKU}_{Cantidad}",
+                        "packdescr": Descripcion,
+                        "packuom1": Tipodepaquete,
+                        "casecnt": Cantidad,
+                        "qty": 1,
+                        "widthuom1": Ancho,
+                        "lengthuom1": Largo,
+                        "heightuom1": Alto,
+                        "weightuom1": Peso,
+                        "pallethi": 1,
+                        "palletti": 1,
+                        "ext_udf_str1": SKU,
+                        "ext_udf_str2": Tipodepaquete
+                    }
+                    print(data)
+                    self.enviar_data(data)
+
+                # Mueve el archivo procesado a la carpeta "procesados"
+                nuevo_nombre = os.path.join(self.carpeta_procesados, os.path.basename(archivo))
+                # Cerrar el archivo antes de intentar moverlo
+                f.close()
+                os.rename(archivo, nuevo_nombre)
+
+                print(f"Archivo procesado: {archivo}")
+
+        except Exception as e:
+            print(f"Error al procesar el archivo {archivo}: {str(e)}")
+
+    def obtener_archivo_mas_antiguo(self, carpeta):
+        archivos = [f for f in os.listdir(carpeta) if f.endswith(".txt")]
+        if not archivos:
+            return None
+        archivos.sort(key=lambda x: datetime.strptime(x.split("_")[1].replace('.txt', ''), "%Y%m%d%H%M%S"))
+        return os.path.join(carpeta, archivos[0])
+
+    def procesar_archivos_continuamente(self):
+        while self.ejecutar:
+            archivo = self.obtener_archivo_mas_antiguo(self.carpeta_archivos)
+            if archivo:
+                self.procesar_archivo(archivo)
+
+
+    def iniciar_proceso(self):
+        # Inicia un hilo para ejecutar el procesamiento en segundo plano
+        self.ejecutar=True
+        Thread(target=self.procesar_archivos_continuamente).start()
+
+    def detener_proceso(self):
+        try:
+            # Detiene el hilo de procesamiento
+            self.ejecutar = False
+            messagebox.showinfo("Proceso detenido", "El proceso ha sido detenido exitosamente.")
+        except Exception as e:
+            messagebox.showerror("Error al detener el proceso", f"Error: {str(e)}")
+
+    def ejecutar_interfaz(self):
+        # Ejecutar la interfaz gráfica
+        root.mainloop()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.resizable(False,False)
+    app = ProcesadorArchivos(root)
+    app.ejecutar_interfaz()
+#comentario prueba
+"""
+# Crear una instancia de la clase y ejecutar la interfaz
+procesador = ProcesadorArchivos()
+procesador.ejecutar_interfaz()
+"""
